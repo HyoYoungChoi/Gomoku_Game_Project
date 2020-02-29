6db92ccb7c5362fdae3a674d8b8c095c7eaa4e49
HY <young8hy@gmail.com>
2020-03-01 02:30:25 +0900

    판정 함수 추가 게임 종료시 초기화

--- Gomoku Project/Client/SinglePlayForm.cs
+++ Gomoku Project/Client/SinglePlayForm.cs
@@ -19,13 +19,58 @@ namespace Client
         private Horse[,] board = new Horse[edgeCount, edgeCount];
         private Horse nowPlayer = Horse.BLACK;  // 보통 흑돌로 시작하기 때문에 초기화값은 BLACK.
 
+        private bool playing = false;   // 시작 중인지 판단.
+
         public SinglePlayForm()
         {
             InitializeComponent();
         }
 
+        private bool judge() // 승리 판정 함수
+        {
+            for (int i = 0; i < edgeCount - 4; i++) // 가로
+                for (int j = 0; j < edgeCount; j++)
+                    if(board[i, j] == nowPlayer && board[i + 1, j] == nowPlayer && board[i + 2, j] == nowPlayer &&
+                        board[i + 3, j] == nowPlayer && board[i + 4, j] == nowPlayer)
+                        return true;
+
+            for (int i = 0; i < edgeCount; i++) //  세로
+                for (int j = 4; j < edgeCount; j++)
+                    if (board[i, j] == nowPlayer && board[i, j - 1] == nowPlayer && board[i, j - 2] == nowPlayer &&
+                        board[i, j - 3] == nowPlayer && board[i, j - 4] == nowPlayer)
+                        return true;
+
+            for (int i = 0; i < edgeCount - 4; i++) // Y = X 직선
+                for (int j = 0; j < edgeCount - 4; j++)
+                    if(board[i, j] == nowPlayer && board[i + 1, j + 1] == nowPlayer && board[i + 2, j+2] == nowPlayer &&
+                        board[i + 3, j+3] == nowPlayer && board[i + 4, j+4] == nowPlayer)
+                        return true;
+
+            for (int i = 4; i < edgeCount; i++) // Y = -X 직선
+                for (int j = 0; j < edgeCount - 4; j++)
+                    if (board[i, j] == nowPlayer && board[i - 1, j + 1] == nowPlayer && board[i - 2, j + 2] == nowPlayer &&
+                        board[i - 3, j + 3] == nowPlayer && board[i - 4, j + 4] == nowPlayer)
+                        return true;
+
+                return false;
+        }
+
+        private void refresh()
+        {
+            this.boardPicture.Refresh();
+            for (int i = 0; i < edgeCount; i++)
+                for (int j = 0; j < edgeCount; j++)
+                    board[i, j] = Horse.none;
+        }
+
         private void boardPicture_MouseDown(object sender, MouseEventArgs e)
         {
+            if(!playing)
+            {
+                MessageBox.Show("게임을 실행해주세요.");
+                return;
+            }
+
             Graphics g = this.boardPicture.CreateGraphics();
             int x = e.X / rectSize;
             int y = e.Y / rectSize;
@@ -35,7 +80,9 @@ namespace Client
                 MessageBox.Show("테두리를 벗어날 수 없습니다.");
                 return;
             }
-            MessageBox.Show(x + ", " + y);
+            if (board[x, y] != Horse.none) return;
+            board[x, y] = nowPlayer;
+
             if(nowPlayer == Horse.BLACK)
             {
                 SolidBrush brush = new SolidBrush(Color.Black);
@@ -47,6 +94,17 @@ namespace Client
                 g.FillEllipse(brush, x * rectSize, y * rectSize, rectSize, rectSize);
             }
 
+            if(judge())
+            {
+                status.Text = nowPlayer.ToString() + "플레이어가 승리했습니다.";
+                playing = false;
+                playButton.Text = "게임시작";
+            }
+            else
+            {
+                nowPlayer = ((nowPlayer == Horse.BLACK) ? Horse.WHITE : Horse.BLACK);
+                status.Text = nowPlayer.ToString() + " 플레이어의 차례입니다.";
+            }
         }
 
         private void boardPicture_Paint(object sender, PaintEventArgs e)
@@ -64,6 +122,22 @@ namespace Client
             {
                 gp.DrawLine(p, rectSize / 2, i, rectSize * edgeCount - rectSize / 2, i);
                 gp.DrawLine(p, i, rectSize / 2, i, rectSize * edgeCount - rectSize / 2);
+            }
+        }
+
+        private void playButton_Click(object sender, EventArgs e)
+        {
+            if(!playing)
+            {
+                refresh();
+                playing = true;
+                playButton.Text = "재시작";
+                status.Text = nowPlayer.ToString() + "플레이어의 차례입니다.";
+            }
+            else
+            {
+                refresh();
+                status.Text = "게임이 재시작되었습니다.";
             }
         }
     }
